993a0df213ab51824e0a502346a475b13bc24401
Nathalie Christmann-Cooper <nathalie@shecodes.co.uk>
2021-01-16 01:55:48 +0000

    Remove gulp task to handle images. Copy damask floral png using eleventy addPassthroughCopy.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const {parallel, watch} = require('gulp');
 
 // Pull in each task
-const images = require('./gulp-tasks/images.js');
 const sass = require('./gulp-tasks/sass.js');
 
 // Set each directory and contents that we want to watch and
@@ -9,12 +8,11 @@ const sass = require('./gulp-tasks/sass.js');
 // prevent the task being run when we run `gulp watch`, but it
 // will run when a file changes.
 const watcher = () => {
-  watch('./src/images/**/*', {ignoreInitial: true}, images);
   watch('./src/scss/**/*.scss', {ignoreInitial: true}, sass);
 };
 
 // The default (if someone just runs `gulp`) is to run each task in parrallel
-exports.default = parallel(images, sass);
+exports.default = parallel(sass);
 
 // This is our watcher task that instructs gulp to watch directories and
 // act accordingly
