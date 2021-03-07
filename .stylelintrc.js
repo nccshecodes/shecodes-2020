0d3ee1f77b94536fe7be225099ab08bb850971ad
Nathalie Christmann-Cooper <nathalie@shecodes.co.uk>
2021-03-07 12:17:49 +0000

    Fixing stylelint errors
    This tidies up sass styles.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,25 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+    'stylelint-config-sass-guidelines',
+  ],
+  rules: {
+    'max-nesting-depth': [
+      2,
+      {
+        ignore: ['pseudo-classes'],
+      },
+    ],
+    'selector-class-pattern': null,
+    'selector-no-qualifying-type': [
+      true,
+      {
+        ignore: ['attribute', 'class'],
+      },
+    ],
+    'scss/at-extend-no-missing-placeholder': null,
+    'scss/at-import-partial-extension-whitelist': ['scss'],
+    'scss/dollar-variable-pattern': null,
+  },
 };
