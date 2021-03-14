839c4cfeccad53092871787f63747a28f56480f9
Nathalie Christmann-Cooper <nathalie@shecodes.co.uk>
2021-03-14 18:35:43 +0000

    Converts all spaces to tabs.

--- .eleventy.js
+++ .eleventy.js
@@ -5,80 +5,80 @@ const mdDeflist = require('markdown-it-deflist');
 const mdAttrs = require('markdown-it-attrs');
 
 module.exports = (config) => {
-  // Set directories to pass through to the dist folder
-  config.addPassthroughCopy('./src/fonts/');
-  config.addPassthroughCopy('./src/images/static/');
-  config.addPassthroughCopy('./src/sitemap.xml');
-  // add filters
-  config.addFilter('dateFilter', dateFilter);
+	// Set directories to pass through to the dist folder
+	config.addPassthroughCopy('./src/fonts/');
+	config.addPassthroughCopy('./src/images/static/');
+	config.addPassthroughCopy('./src/sitemap.xml');
+	// add filters
+	config.addFilter('dateFilter', dateFilter);
 
-  // Shortcodes
-  config.addNunjucksAsyncShortcode('imageShortcode', imageShortcode);
+	// Shortcodes
+	config.addNunjucksAsyncShortcode('imageShortcode', imageShortcode);
 
-  // Transforms
-  const htmlMinTransform = require('./src/transforms/html-min-transform.js');
-  const isProduction = process.env.NODE_ENV === 'production';
+	// Transforms
+	const htmlMinTransform = require('./src/transforms/html-min-transform.js');
+	const isProduction = process.env.NODE_ENV === 'production';
 
-  // Only minify HTML if we are in production because it slows builds _right_ down
-  if (isProduction) {
-    config.addTransform('htmlmin', htmlMinTransform);
-  }
+	// Only minify HTML if we are in production because it slows builds _right_ down
+	if (isProduction) {
+		config.addTransform('htmlmin', htmlMinTransform);
+	}
 
-  config.addCollection('cv_awards', (collection) => {
-    return collection.getFilteredByGlob('./src/awards/*.md').reverse();
-  });
+	config.addCollection('cv_awards', (collection) => {
+		return collection.getFilteredByGlob('./src/awards/*.md').reverse();
+	});
 
-  config.addCollection('blog', (collection) => {
-    return [...collection.getFilteredByGlob('./src/posts/**/*.md')].reverse();
-  });
+	config.addCollection('blog', (collection) => {
+		return [...collection.getFilteredByGlob('./src/posts/**/*.md')].reverse();
+	});
 
-  config.addCollection('cv_education', (collection) => {
-    return collection.getFilteredByGlob('./src/education/*.md').reverse();
-  });
+	config.addCollection('cv_education', (collection) => {
+		return collection.getFilteredByGlob('./src/education/*.md').reverse();
+	});
 
-  config.addCollection('cv_jobs', (collection) => {
-    return collection.getFilteredByGlob('./src/jobs/*.md').reverse();
-  });
+	config.addCollection('cv_jobs', (collection) => {
+		return collection.getFilteredByGlob('./src/jobs/*.md').reverse();
+	});
 
-  config.addCollection('cv_talks', (collection) => {
-    return collection.getFilteredByGlob('./src/talks/*.md').reverse();
-  });
+	config.addCollection('cv_talks', (collection) => {
+		return collection.getFilteredByGlob('./src/talks/*.md').reverse();
+	});
 
-  config.addCollection('tagList', (collection) => {
-    const tagsSet = new Set();
-    collection.getAll().forEach((item) => {
-      if (!item.data.tags) return;
-      item.data.tags
-        .filter((tag) => !['all'].includes(tag))
-        .forEach((tag) => tagsSet.add(tag));
-    });
-    return Array.from(tagsSet).sort();
-  });
+	config.addCollection('tagList', (collection) => {
+		const tagsSet = new Set();
+		collection.getAll().forEach((item) => {
+			if (!item.data.tags) return;
+			item.data.tags
+				.filter((tag) => !['all'].includes(tag))
+				.forEach((tag) => tagsSet.add(tag));
+		});
+		return Array.from(tagsSet).sort();
+	});
 
-  config.setUseGitIgnore(false);
+	config.setUseGitIgnore(false);
 
-  // customise and extend markdown
-  // mdAttr - add ids and classes
-  // mdDefList - create description lists
-  const options = {
-    html: true,
-  };
+	// customise and extend markdown
+	// mdAttr - add ids and classes
+	// mdDefList - create description lists
+	const options = {
+		html: true,
+	};
 
-  const markdownLib = mdIt(options)
-    .use(mdAttrs, {
-      allowedAttributes: ['class', 'id', /^aria.*$/],
-    })
-    .use(mdDeflist);
+	const markdownLib = mdIt(options)
+		.use(mdAttrs, {
+			allowedAttributes: ['class', 'id', /^aria.*$/],
+		})
+		.use(mdDeflist);
 
-  config.setLibrary('md', markdownLib);
+	config.setLibrary('md', markdownLib);
 
-  return {
-    markdownTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
-    dir: {
-      input: 'src',
-      output: 'dist',
-    },
-  };
+	return {
+		markdownTemplateEngine: 'njk',
+		dataTemplateEngine: 'njk',
+		htmlTemplateEngine: 'njk',
+		dir: {
+			input: 'src',
+			output: 'dist',
+		},
+	};
 };
--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -1,47 +1,47 @@
 module.exports = {
-  /**
-   * Returns back some attributes based on whether the
-   * link is active or a parent of an active item
-   *
-   * @param {String} itemUrl The link in question
-   * @param {String} pageUrl The page context
-   * @returns {String} The attributes or empty string
-   */
-  getLinkActiveState(itemUrl, pageUrl) {
-    let response = '';
+	/**
+	 * Returns back some attributes based on whether the
+	 * link is active or a parent of an active item
+	 *
+	 * @param {String} itemUrl The link in question
+	 * @param {String} pageUrl The page context
+	 * @returns {String} The attributes or empty string
+	 */
+	getLinkActiveState(itemUrl, pageUrl) {
+		let response = '';
 
-    if (itemUrl === pageUrl) {
-      response = ' aria-current="page"';
-    }
+		if (itemUrl === pageUrl) {
+			response = ' aria-current="page"';
+		}
 
-    if (itemUrl.length > 1 && pageUrl.indexOf(itemUrl) === 0) {
-      response += ' data-state="active"';
-    }
+		if (itemUrl.length > 1 && pageUrl.indexOf(itemUrl) === 0) {
+			response += ' data-state="active"';
+		}
 
-    return response;
-  },
+		return response;
+	},
 
-  /**
-   * Filters out item (usually latest post) from the passed collection
-   * and limits number of items returned based on flag
-   *
-   * @param {Array} collection The 11ty collection
-   * @param {Object} item The item to exclude
-   * @param {Number} limit How many items to return. Default of zero does not apply a limit.
-   * @returns {Array} The resulting collection
-   */
-  getSiblingContent(collection, item, limit = 0) {
-    if (!collection) return null;
-    let filteredItems = collection.filter((x) => x.date !== item.date);
+	/**
+	 * Filters out item (usually latest post) from the passed collection
+	 * and limits number of items returned based on flag
+	 *
+	 * @param {Array} collection The 11ty collection
+	 * @param {Object} item The item to exclude
+	 * @param {Number} limit How many items to return. Default of zero does not apply a limit.
+	 * @returns {Array} The resulting collection
+	 */
+	getSiblingContent(collection, item, limit = 0) {
+		if (!collection) return null;
+		let filteredItems = collection.filter((x) => x.date !== item.date);
 
-    if (limit > 0) {
-      filteredItems = filteredItems.slice(-limit);
-    }
+		if (limit > 0) {
+			filteredItems = filteredItems.slice(-limit);
+		}
 
-    return filteredItems;
-  },
+		return filteredItems;
+	},
 
-  getYear() {
-    return new Date().getFullYear();
-  },
+	getYear() {
+		return new Date().getFullYear();
+	},
 };
